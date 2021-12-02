5fbfef17ff07e33b422dc9618366f580478e7b57
Jonas Berg <jonas.berg@relex.fi>
2021-12-02 18:58:55 +0200

    Initial solutions to days 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var lastVal = input[0].toInt()
+        var returnVal = 0
+        for (num in input.subList(1, input.size)) {
+            if (num.toInt() > lastVal) {
+                returnVal++
+            }
+            lastVal = num.toInt()
+        }
+        return returnVal
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var triplet = input[0].toInt() + input[1].toInt() + input[2].toInt()
+        var increases = 0
+        for (idx in 3 until input.size) {
+            val newTriplet = triplet + input[idx].toInt() - input[idx-3].toInt()
+            if (newTriplet > triplet) {
+                increases++
+            }
+            triplet = newTriplet
+        }
+        return increases
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(testInput[testInput.size-1] == "177")
+    check(part1(testInput) == 2)
 
+    val testInput2 = readInput("Day01_test2")
+    check(part1(testInput2) == 7)
+    check(part2(testInput2) == 5)
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
---
+++ src/Day02.kt
@@ -0,0 +1,43 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var pos = 0
+        var depth = 0
+        for (line in input) {
+            val splitLine = line.split(' ')
+            val (direction, size) = Pair(splitLine[0], splitLine[1])
+            when (direction) {
+                "forward" -> pos += size.toInt()
+                "up" -> depth -= size.toInt()
+                "down" -> depth += size.toInt()
+            }
+        }
+        return pos * depth
+    }
+
+    fun part2(input: List<String>): Int {
+        var pos = 0
+        var depth = 0
+        var aim = 0
+        for (line in input) {
+            val splitLine = line.split(' ')
+            val (direction, size) = Pair(splitLine[0],splitLine[1])
+            when (direction) {
+                "forward" -> { pos += size.toInt()
+                    depth += aim * size.toInt() }
+                "up" -> aim -= size.toInt()
+                "down" -> aim += size.toInt()
+            }
+        }
+        return pos * depth
+    }
+
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
+
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
+
